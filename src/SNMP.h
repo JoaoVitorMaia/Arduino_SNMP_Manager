37b40e7fbef400c3d3532309d5e4cd49dad6dba7
Joao <joaovitormaia@protonmail.com>
2022-10-06 13:03:09 -0300

    implemented another response types

--- src/SNMP.h
+++ src/SNMP.h
@@ -13,7 +13,9 @@ class SNMP
     WiFiUDP _udp;
     ValueCallback *_callback;
     public:
-        SNMP(const char *community, short snmpVersion) : _community(community), _version(snmpVersion){}
+        SNMP(const char *community, short snmpVersion) : _community(community), _version(snmpVersion){
+        }
+        //TODO: throw exception when get no response from oid
         String getString(const char *oid, short int timeout, IPAddress targetIp)
         {
             int time = millis();
@@ -24,14 +26,52 @@ class SNMP
             _snmp.begin();
             _callback = _snmp.addStringHandler(targetIp, oid, &valueResponse);
             send(targetIp);
-            _snmp.loop();
             while(!_snmp.loop()){
                 if((millis()-time)/1000 >= timeout)
                     return "";
             }
             return String(valueResponse);
         }
-
+        int getInt(const char *oid, short int timeout, IPAddress targetIp)
+        {
+            int time = millis();
+            int value;
+            _callback = _snmp.addIntegerHandler(targetIp, oid, &value);
+            send(targetIp);
+            while(!_snmp.loop()){
+                if((millis()-time)/1000 >= timeout)
+                    return 0;
+            }
+            return value;
+        }
+        
+        float getFloat(const char *oid, short int timeout, IPAddress targetIp){
+            int time=millis();
+            float value;
+            _callback = _snmp.addFloatHandler(targetIp, oid, &value);
+            send(targetIp);
+            while(!_snmp.loop()){
+                if((millis()-time)/1000 >= timeout)
+                    return 0.0;
+            }
+            return value;
+        }
+        //response on ticks
+        uint32_t getTimestamp(const char *oid, short int timeout, IPAddress targetIp){
+            int time = millis();
+            uint32_t value;
+            _callback = _snmp.addTimestampHandler(targetIp, oid, &value);
+            send(targetIp);
+            while(!_snmp.loop()){
+                if((millis()-time)/1000 >= timeout)
+                    return 0.0;
+            }
+            return value;
+        }
+        OIDCallback getOid(const char *oid, short int timeout, IPAddress targetIp);
+        uint64_t getCounter64(const char *oid, short int timeout, IPAddress targetIp);
+        uint32_t getCounter43(const char *oid, short int timeout, IPAddress targetIp);
+        uint32_t getGuage(const char *oid, short int timeout, IPAddress targetIp);
         bool loop(){
             return _snmp.loop();
         }
