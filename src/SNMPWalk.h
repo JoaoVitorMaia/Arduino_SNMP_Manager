c4190c39dbcb103bb1942b0d81cbcf8b61cf185c
Joao <joaoc4rp1n4t0r@protonmail.ch>
2022-12-06 13:48:38 -0300

    Added walk function

--- src/SNMPWalk.h
+++ src/SNMPWalk.h
@@ -11,13 +11,13 @@ class SNMPWalk
     WiFiUDP _udp;
     SNMPManager manager;
     SNMPGetNext snmpRequest;
-    const char *startOID = ".1";
+    const char *startOID = ".1.3.6.1.2.1.1.2.0";
 
 public:
     SNMPWalk(const char *community, short snmpVersion) : _community(community), _version(snmpVersion)
     {
     }
-    void walk(IPAddress ip)
+    ValueCallback *walk(IPAddress ip)
     {
         manager = SNMPManager(_community);
         snmpRequest = SNMPGetNext(_community, _version);
@@ -33,23 +33,60 @@ public:
         while (!manager.loop())
         {
         }
-        if (manager.results->value)
+        while (true)
         {
-            switch (manager.results->value->type)
+            ValueCallbacks *resultsCursor = manager.results;
+            if (resultsCursor->value)
             {
-            case STRING:
-
-                Serial.println(((StringCallback *)manager.results->value)->value);
-                break;
-
-            default:
-            Serial.print(manager.callbacksCursor->value->type);
-                break;
+                while (resultsCursor->next != 0)
+                {
+                    resultsCursor = resultsCursor->next;
+                }
+                callback = manager.addNextRequestHandler(ip, resultsCursor->value->OID);
+                snmpRequest.addOIDPointer(callback);
+                snmpRequest.setRequestID(rand() % 5555);
+                snmpRequest.sendTo(ip);
+                snmpRequest.clearOIDList();
+                while (!manager.loop())
+                {
+                }
             }
-        }else{
-            Serial.println("no value");
         }
-        Serial.println(((StringCallback *)manager.results->value)->value);
+        /*ValueCallbacks *resultsCursor = manager.results;
+        if (resultsCursor->value)
+        {
+            while (resultsCursor->next != 0)
+            {
+                switch (resultsCursor->value->type)
+                {
+                case STRING:
+                    Serial.println(((StringCallback *)manager.results->value)->value);
+                    break;
+                case INTEGER:
+                    callback = addHandler(responseIP, responseOID, INTEGER);
+                    break;
+                case OID:
+                    callback = addHandler(responseIP, responseOID, OID);
+                    break;
+                case COUNTER32:
+                    callback = addHandler(responseIP, responseOID, COUNTER32);
+                    break;
+                case GAUGE32:
+                    callback = addHandler(responseIP, responseOID, GAUGE32);
+                    break;
+                case TIMESTAMP:
+                    callback = addHandler(responseIP, responseOID, TIMESTAMP);
+                    break;
+                case COUNTER64:
+                    callback = addHandler(responseIP, responseOID, COUNTER64);
+                default:
+                    break;
+                default:
+                    Serial.print(manager.callbacksCursor->value->type);
+                    break;
+                }
+            }
+        }*/
     }
 };
 #endif
